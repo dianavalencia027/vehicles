86bf93a1f4286e60640f2c6ac4a7d3efa5f8f71a
Diana Valencia <dianavalencia027@gmail.com>
2021-08-31 21:14:38 -0500

    004 - Procedures

--- Vehicles.API/Data/DataContext.cs
+++ Vehicles.API/Data/DataContext.cs
@@ -9,9 +9,13 @@ namespace Vehicles.API.Data
 {
     public class DataContext : DbContext
     {
+        internal object DocumentTypes;
+
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
         }
+
+        public DbSet<Procedure> Procedures { get; set; }
 
         public DbSet<VehicleType> VehicleTypes { get; set; }
 
@@ -19,6 +23,7 @@ namespace Vehicles.API.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Procedure>().HasIndex(x => x.Description).IsUnique();
             modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();
         }
     }
