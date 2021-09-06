3917e3047e47faedcf88dcd04cfd70838027da17
Diana Valencia <dianavalencia027@gmail.com>
2021-09-06 17:04:55 -0500

    011 - BlobHelper IBlobHelper ConverterHelper IConverterHelper UserViewModel

--- Vehicles.API/Startup.cs
+++ Vehicles.API/Startup.cs
@@ -43,6 +43,8 @@ namespace Vehicles.API
             services.AddTransient<SeedDb>();
             services.AddScoped<IUserHelper, UserHelper>();
             services.AddScoped<ICombosHelper, CombosHelper>();
+            services.AddScoped<IBlobHelper, BlobHelper>();
+            services.AddScoped<IConverterHelper, ConverterHelper>();
         }
 
 
